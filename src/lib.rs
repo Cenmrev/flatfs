Cenmrev/flatfs

commit 90481d63a7278ba8f71fa8b639d75cfb3877661e

Add the full path to struct Dir.

src/lib.rs
@@ -116,7 +116,8 @@ impl Writer {
 }
 
 pub struct Dir<'a> {
-    pub name: &'a str,
+    pub filename: &'a str, // The leaf filename.
+    pub path: String,      // The full path.
     pub subdirs: BTreeMap<&'a str, Dir<'a>>,
     pub files: BTreeMap<&'a str, &'a [u8]>,
 }
@@ -140,10 +141,14 @@ fn add_file<'a>(dir: &'_ mut Dir<'a>, name: &'a str, bytes: &'a [u8]) -> Result<
     let child: &mut Dir<'a> = if let Some(d) = dir.subdirs.get_mut(left) {
         d
     } else {
+        let mut new_path = dir.path.clone();
+        new_path.push('/');
+        new_path.push_str(left);
         dir.subdirs.insert(
             left,
             Dir {
-                name: left,
+                filename: left,
+                path: new_path,
                 subdirs: BTreeMap::new(),
                 files: BTreeMap::new(),
             },
@@ -232,7 +237,8 @@ pub fn unpack<'a>(buf: &'a [u8]) -> Result<Dir<'a>, ()> {
 
     // Now parse files into a directory tree.
     let mut root_dir = Dir {
-        name: "/",
+        filename: "",
+        path: "".to_owned(),
         subdirs: BTreeMap::new(),
         files: BTreeMap::new(),
     };

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "flatfs"
-version = "0.1.1"
+version = "0.2.0"
 edition = "2021"
 description = "A readonly filesystem."
 repository = "https://github.com/lasiotus/flatfs"

commit c720b943e94443c7933c87924bd84dc85bef3388

Fix unit test
Fixes: 90481d63a727 ("Add the full path to struct Dir.")

src/lib.rs
@@ -261,7 +261,7 @@ fn test() {
     let flat_fs = writer.pack();
 
     let root_dir = unpack(&flat_fs).unwrap();
-    assert_eq!(root_dir.name, "/");
+    assert_eq!(root_dir.filename, "");
 
     assert_eq!(root_dir.files.len(), 1);
     assert_eq!(root_dir.files.get("foo").unwrap(), b"foo");